563c5710ab72c4db2f6799a86443007a501705c3
Renjith S Raj <renjiths_r@CHMB122690.local>
2019-11-20 16:01:23 +0530

    update all

--- blog/models.py
+++ blog/models.py
@@ -1,41 +1,36 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import AbstractUser
-
-# class User(AbstractUser):
-#     bio = models.TextField(max_length=500, blank=True)
-    
-#     def __str__(self):
-#         pass
-
-#     class Meta:
-#         db_table = ''
-#         managed = True
-#         verbose_name = 'User'
-#         verbose_name_plural = 'Users'
+from ckeditor.fields import RichTextField
+from django.urls import reverse
 
 
-
-class TimeStampModel(models.Model):
-    created = models.DateTimeField(auto_now_add=True, auto_now=False)
-    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
-    
-    def __str__(self):
-        pass
+class TimeStampModel (models.Model):
+    created = models.DateTimeField (auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField (auto_now_add=False, auto_now=True)
 
     class Meta:
         db_table = ''
         managed = True
         verbose_name = 'TimeStampModel'
         verbose_name_plural = 'TimeStampModels'
-        
-        
+
+
+class User(AbstractUser):
+    bio = RichTextField()
+    nick_name = models.CharField(max_length=200, null=True, blank=True)
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+
 class Tags(TimeStampModel):
     name = models.CharField(verbose_name= _('Tag Name'), max_length=120)
     is_active = models.BooleanField(verbose_name=_('Active Or Not?'))
-    
-    def __str__(self):
-        return self.name
+
 
     class Meta:
         db_table = ''
@@ -44,11 +39,12 @@ class Tags(TimeStampModel):
         verbose_name_plural = 'Tagss'
 
 
-class Category(TimeStampModel):
+class Category(models.Model):
     name = models.CharField(verbose_name=_('Catory Name'), max_length=120)
     is_active = models.BooleanField(verbose_name=_('Active Or Not?'))
-    def __str__(self):
-        return self.name
+
+    # def __str__(self):
+    #     return self.name
 
     class Meta:
         db_table = ''
@@ -56,15 +52,58 @@ class Category(TimeStampModel):
         verbose_name = 'Category'
         verbose_name_plural = 'Categorys'
 
+
+class BlogQuerySet(models.QuerySet):
+
+    def published(self):
+        return self.filter(publish=True)
+
+
 class Blog(TimeStampModel):
-    title = models.CharField(verbose_name=_('Blog Title'), max_length=110)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name=_('Category'))
+    author = models.ForeignKey(User, verbose_name=_('Author'), related_name='user_blogs',
+                               on_delete=models.CASCADE)
+    title = models.CharField(verbose_name=_('Blog Title'), max_length=200, null=True, blank=True)
+    slug = models.SlugField(verbose_name=_('Slug'), unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name=_('Category'),
+                                 related_name='blogs')
     tags = models.ManyToManyField(Tags, verbose_name=_('Tag'))
+    description = RichTextField()
+    short_content = models.TextField(verbose_name=_('Short Content'))
+    reading_time = models.CharField(max_length=30, verbose_name=_('Reading Time'), null=True, blank=True)
+
+    # SEO details
+    meta_description = models.CharField(verbose_name=_('Meta Description'), max_length=150, null=True,
+                                        blank=True)
+    meta_tags = models.TextField(verbose_name=_('Meta Tags(sep in ,)'))
+    image_url = models.URLField(verbose_name=_('Image URL'), null=True, blank=True)
+    video_url = models.URLField(verbose_name=_('Video URL'), null=True, blank=True)
+
+    published = models.BooleanField(default=False, verbose_name=_('Published ?'))
+    order_by = models.IntegerField(default=1, verbose_name=_('Blog Order'))
+    visitor_count = models.IntegerField(default=1, verbose_name=_('Visitor Count'))
     # user = models
 
+    is_trending = models.BooleanField(default=False, verbose_name=_('Trending?'))
+    is_featured = models.BooleanField(default=False, verbose_name=_('Featured?'))
+
+    def get_absolute_path(self):
+        return reverse('detail_post_page', kwargs={'slug': str(self.slug)})
+
+    # def default_image_url(self):
+    #     if not self.image_url:
+    #         return "Default"
+    #     return self.image_url
+
+    # def update_count(self):
+    #     blog = Blog.objects.get(id=self.id)
+    #     blog.visitor_count += 1
+    #     blog.save()
 
     def __str__(self):
-        return 
+        return str(self.title) if self.title else ''
 
-    def __unicode__(self):
-        return 
+
+
+
+
+
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -131,4 +131,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+# Static config in django
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticstorage")
+
+AUTH_USER_MODEL = 'blog.User'
+
